767a08edfbcb8ce649f0d6d381b118a0cc94440a
oden <oden6680@gmail.com>
2023-02-20 05:47:17 +0900

    作詞者ベースで情報を取得

--- liella_lyric/liella_lyric/spiders/lyryc.py
+++ liella_lyric/liella_lyric/spiders/lyryc.py
@@ -4,12 +4,14 @@ import scrapy
 class LyricSpider(scrapy.Spider):
     name = "lyric"
     allowed_domains = ["www.uta-net.com"]
-    start_urls = ["https://www.uta-net.com/artist/30181/5/"]
+    start_urls = ["https://www.uta-net.com/lyricist/28370/"]
 
     def parse(self, response):
-        for href in response.css('#list-song > div.row.justify-content-left.mt-lg-4 > div.main.col-sm-12.col-lg-8.flex-grow-1.ms-lg-0.pe-lg-1 > div.row.songlist-table-block.p-3 > div.py-2.p-0.p-lg-2 > table > tbody > tr > td.sp-w-100.pt-0.pt-lg-2 > a::attr(href)'):
-            url = 'https://www.uta-net.com' + href.get()
-            yield scrapy.Request(url, callback=self.parse_item)
+        for i in range(4):
+            table = '#list-song > div.row.justify-content-left.mt-lg-4 > div.main.col-sm-12.col-lg-8.flex-grow-1.ms-lg-0.pe-lg-1 > div.row.songlist-table-block.p-3 > div.py-2.p-0.p-lg-2 > table:nth-child(' + str(i+1) + ')'
+            for href in response.css(table + ' > tbody > tr > td.sp-w-100.pt-0.pt-lg-2 > a::attr(href)'):
+                url = 'https://www.uta-net.com' + href.get()
+                yield scrapy.Request(url, callback=self.parse_item)
     
     def parse_item(self, response):
         yield {
